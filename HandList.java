3e0d8627bc2492b4cb548e96c46a320d71440421
MdRafsanZaman <mrz281@nyu.edu>
2025-12-07 19:25:51 -0500

    New Useful CardList type added

---
+++ HandList.java
@@ -0,0 +1,14 @@
+public class HandList<E> extends CardList<E>{
+
+    // All cards in such a list have the same hand
+    String hand;
+
+    public HandList(String hand){
+
+        super();
+        this.hand = hand;
+
+    }
+
+    
+}
